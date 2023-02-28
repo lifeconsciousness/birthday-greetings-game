03c7aeee9b57aa1aa282296667e39ea3a3761509
lifeconsciousness <lifeconsciousness321@gmail.com>
2023-02-28 14:28:24 +0100

    loading animation, birthday greeting animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import './index.scss'
-import Balloon from './components/Balloon'
+import { useState, useEffect } from 'react'
+import './index.scss' 
 
 function App() {
-  const [count, setCount] = useState(0)
-  //find some cool gradient text (hyperplexed) and make baloons animation
-  //first make baloons or particles, then text
-  //add a small song to the file
-  //add play button
-  //baloons are released, text is animated and song is played
-  //make jingle sound that plays after popping the balloon
-  const releaseBalloons = () => {
-    for(let i = 0; i < 9; i++){
-      <Balloon />
-    }
-  }
+  const [loading, setLoading] = useState(false)
+  useEffect(()=> {
+    setLoading(true)
+  }, [])
+  
+  //the balloon pops and fireworks animation starts playing and text pops out
+  const onBalloonPop = () => {
+    console.log('pop')
+  };
 
   return (
     <div className="App">
-      <div className="box" style={{zIndex: -3, top: '15px'}}></div>
+      <div className={loading ? "loading-screen fadeOutAnimation" : 'loading-screen'}>
+        <div className="lines-container">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+        <h2 className='loading-message'>Loading</h2>
+        <div className="lines-container mirrorToRight">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </div>
 
-      <h1 className="congratulation-text">Happy birthday</h1>
+      <div className="wave" style={{zIndex: -3, top: '15px'}}></div>
+      <h1 className="congratulation-text">Happy birthday!</h1>
 
-      <Balloon />
+
+      <div className="balloonContainer" 
+        onClick={()=>{onBalloonPop()}}
+        style={
+          {left: `${Math.floor(Math.random() * 60)+13}vw`,
+            animationDelay: `${(Math.floor(Math.random() * 10) + 1)/10}s`,
+          }}>
+        <div className="balloon"><div className="balloonShade"></div></div>
+        <div className="rope"></div>
+      </div>
       
 
       <div className="firework" style={{transform: 'translate(-400px, -100px)', animationDelay: '.3s'}}></div>
       <div className="firework"></div>
+      <div className="firework"></div>
+      <div className="firework"></div>
       <div className="firework" style={{transform: 'translate(300px, -200px)', animationDelay: '.1s'}}></div>
 
-      <div className="box" style={{bottom: '20px', zIndex: 888}}></div>
+      <div className="wave" style={{bottom: '20px', zIndex: 888}}></div>
+
+
+
+
+      <div className="game">
+        
+      </div>
+
     </div>
   )
 }
