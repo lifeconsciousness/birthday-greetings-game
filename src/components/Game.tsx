613a57d58d809b6cafea327a09a37d94d1d56704
lifeconsciousness <lifeconsciousness321@gmail.com>
2023-03-19 00:53:57 +0100

    defeat/win/restart system. the game is working

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,20 +1,11 @@
-import React from 'react'
 import '../index.scss'
 import { useEffect, useState, useRef, memo } from 'react'
-import ReplayBtn from '/replay.png'
+import RestartBtn from '/restart.png'
 import Present from './Present'
 
 type Props = {
 
 }
-
-//write message to pop the balloon and message about controlls in the game
-//make the initial 3,2,1 countdown
-//make squares randomly generate on the top and fall with different velocity
-//draw player stickman with hands trying to catch the present
-//find/draw texture for presents and stage
-//add animation to the character when it moves
-//add some music and sfx
 
 export default function Game({ }: Props) {
 
@@ -119,21 +110,15 @@ export default function Game({ }: Props) {
         animationLength: number,
     }
     //0 is game, -1 - defeat, 1 - wictory
-    const [win, setWin] = useState(-1)
+    const [win, setWin] = useState(0)
 
     const [presents, setPresents] = useState<Present[]>([])
     const [firstTime, setFirstTime] = useState(true)
     const [score, setScore] = useState(0)
-    const spawnDelay = Math.floor(Math.random() * 2300) + 1500
-    // let presentAmount = Math.floor(Math.random() * 5) + 1
-    let presentAmount = 0
+    const [spawnDelay, setSpawnDelay] = useState(Math.floor(Math.random() * 2500) + 1000)
+    const presentAmount = Math.floor(Math.random() * 3) + 2
 
-    if (firstTime) { generatePresents(); setFirstTime(false) }
 
-    //////////////////////////Reseting the game//////////////////////////////////
-    if (score < 0) {
-        presentAmount = 0
-    }
     ////////////////////////Generating and deleting presents////////////////////
 
     function generatePresents() {
@@ -145,7 +130,6 @@ export default function Game({ }: Props) {
                     id: Math.random().toString(32).substring(2, 9),
                     left: Math.floor(Math.random() * 70) + 10,
                     size: Math.floor(Math.random() * 50) + 100,
-                    // size: Math.floor(Math.random() * 2) + 12,
                     animationDelay: Math.floor(Math.random() * 4) + 1,
                     animationLength: Math.floor(Math.random() * 5) + 2,
                 })
@@ -154,15 +138,17 @@ export default function Game({ }: Props) {
         setPresents((prevPresents) => [...prevPresents, ...newPresents])
     }
 
-    //constantly generating the presents
+    //constantly generating the presents and checking if the player lost the game
     useEffect(() => {
         const interval = setInterval(() => {
-            generatePresents()
+            if (win === 0) {
+                generatePresents()
+            }
         }, spawnDelay)
 
         //clearing the interval to prevent memory leaks
         return () => clearInterval(interval)
-    }, [])
+    }, [win])
 
     //check presents if they reached the certain position
     useEffect(() => {
@@ -177,7 +163,7 @@ export default function Game({ }: Props) {
 
                 //if present reaches the bottom it is deleted
                 if (presentRect && presentRect.top > window.innerHeight - 100) {
-                    setScore((prevScore) => prevScore - 10)
+                    setScore((prevScore) => prevScore - 8)
                     return false
                 }
                 //if present touches player it is deleted
@@ -193,6 +179,20 @@ export default function Game({ }: Props) {
         return () => clearInterval(interval)
     }, [])
 
+    ////////////////////////stopping and reseting the game/////////////////////
+    useEffect(() => {
+        if (score < 0) {
+            setWin(-1)
+            setPresents([])
+            setPlayerPosition(windowWidth / 2)
+        }
+        if (score >= 0) {
+            setWin(0)
+        } else if (score > 512) {
+            setWin(1)
+        }
+    }, [score])
+
     //////////////////////Main game components put together//////////////////////////
     return (
         <>
@@ -207,45 +207,27 @@ export default function Game({ }: Props) {
                 <img className="player" ref={playerRef} style={{ left: `${playerPosition}px` }}></img>
                 <div className="stage"></div>
                 <h2 className='score'>Score: {score}</h2>
-                <h1 className="win-message">{
-                    win === -1 ? 'You lost' :
-                        win === 1 ? 'Wow, you either modified the code or you are a persistent one. In any case, my congratulations!' : ''
-                }</h1>
-                <img src={ReplayBtn} alt="replay button" className='replay-button' />
+                <div className="info-and-restart">
+                    <h1 className="win-message">{
+                        win === -1 ? 'You lost' :
+                            win === 1 ? 'Wow, you either modified the code or you are a persistent one. In any case, my congratulations!' : ''}
+                    </h1>
+                    <img src={RestartBtn}
+                        alt="restart button"
+                        className='restart-button'
+                        onClick={() => setScore(0)}
+                        style={{ display: win === -1 ? 'block' : win === 1 ? 'block' : 'none' }}
+                    />
+                </div>
             </div>
         </>
     )
 }
 
 
-
-
-
-// type PropsPresent = {
-//     htmlId: string,
-//     left: number,
-//     size: number,
-//     animationDelay: number,
-//     animationLength: number,
-// }
-
-// //react memo to persist player position through renders
-// const Present = React.memo(({ htmlId, left, size, animationDelay, animationLength }: PropsPresent) => {
-//     return (
-//         <div
-//             id={`present-${htmlId}`}
-//             className="present"
-//             style={{
-//                 animation: `present-fall ${animationLength}s`,
-//                 width: `${size}px`,
-//                 left: `${left}vw`,
-//                 animationDelay: `${animationDelay}s`,
-//             }}>
-//         </div>
-//     )
-// })
-
-
-//defeat logic and restart button
-//animated running stickman
-//textures and css
+//try to remove the delay before presents spawn
+//present rain
+//draw player stickman with hands trying to catch the present
+//find/draw texture for presents and stage
+//add animation to the character when it moves
+//add some music and sfx
